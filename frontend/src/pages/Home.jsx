5f3cd341095c1af9a0f55b8191c8f7a84c4cfb3c
SuyogJo <suyoghero@gmail.com>
2023-10-12 19:58:08 -0400

    added backbutton to return to homepage and added list of books in the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,13 +15,62 @@ export default function Home() {
   useEffect(() => {
     setLoading(true);
     axios.get('http://localhost:5000/books').then(
-      (response) => console.log(response)
-    )
+      (response) => {
+        setBooks(response.data.data)
+        setLoading(false)
+      }
+    ).catch((error) => {
+      console.log(error);
+      setLoading(false)
+    })
   }, []);
 
   return (
-    <div>
-      HomePage
+    <div className='p4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-3xl my-8'>Book List</h1>
+        <Link to='/books/create'>
+          <MdOutlineAddBox className='text-sky-800 text-4xl'></MdOutlineAddBox>
+        </Link>
+      </div>
+      {loading ? <Spinner/> : 
+        (
+          <table className='w-full border-seperate-2'>
+            <thead>
+              <tr>
+                <th className='border border-slate-600 rounded-md'>No</th>
+                <th className='border border-slate-600 rounded-md'>Title</th>
+                <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
+                <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
+                <th className='border border-slate-600 rounded-md'>Operations</th>
+              </tr>
+            </thead>
+            <tbody>
+              {books.map((book) => 
+                <tr key={book._id} className='h-8'>
+                  <th className='border border-slate-700 rounded-md text-center'>{books.indexOf(book)+1}</th>
+                  <th className='border border-slate-700 rounded-md text-center'>{book.title}</th>
+                  <th className='border border-slate-700 rounded-md text-center max-md:hidden' >{book.author}</th>
+                  <th className='border border-slate-700 rounded-md text-center max-md:hidden'>{book.publishYear}</th>
+                  <th className='border border-slate-700 rounded-md text-center'>
+                    <div className='flex justify-center gap-x-4'>
+                      <Link to={`/books/show/${book._id}`}>
+                        <BsInfoCircle className='text-2x1 text-green-800' />
+                      </Link>
+                      <Link to={`/books/edit/${book._id}`}>
+                        <AiOutlineEdit className='text-2x1 text-yellow-800' />
+                      </Link>
+                      <Link to={`/books/delete/${book._id}`}>
+                        <MdOutlineDelete className='text-2x1 text-red-800' />
+                      </Link>
+                    </div>
+                  </th>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        )  
+      }
     </div>
   )
 }
--- frontend/src/pages/ShowBooks.jsx
+++ frontend/src/pages/ShowBooks.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import {useParams} from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
 
 export default function ShowBook() {
+  const [book, setBook] = useState([])
   return (
     <div>
       Show
