f8d5af227212f69e3d4fbd1b60010746f69e5e94
SuyogJo <suyoghero@gmail.com>
2023-10-13 20:32:37 -0400

    added functionality to delete a book

--- frontend/src/pages/DeleteBooks.jsx
+++ frontend/src/pages/DeleteBooks.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function DeleteBooks() {
+  
+  const [loading, setLoading] = useState(true)
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDeleteBook = () => {
+    axios.delete(`http://localhost:5000/books/${id}`)
+    .then(() => {
+      setLoading(false)
+      navigate('/')
+    })
+    .catch((error) => {
+      setLoading(false)
+      console.log(error)
+    })
+  }
+
   return (
     <div>
-      Delete
+      {handleDeleteBook()}
     </div>
   )
 }
